55cf6f49dd845d1bd69bdd21f64a636b4b5af992
nisarg78 <npzaveri008@gmail.com>
2026-06-10 18:25:07 -0400

    feat: add NextAuth config with Google OAuth2 and Gmail scopes

---
+++ src/auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: [
+            "openid",
+            "email",
+            "profile",
+            "https://www.googleapis.com/auth/gmail.readonly",
+            "https://www.googleapis.com/auth/gmail.labels",
+            "https://www.googleapis.com/auth/gmail.modify",
+          ].join(" "),
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken ?? ""
+      session.refreshToken = token.refreshToken ?? ""
+      return session
+    },
+  },
+})
